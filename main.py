9d0dac0bd7533f08e5c1c3b6d80eecabfbd3cef4
sibir007 <sibriakoff2006@yandex.ru>
2024-12-12 15:48:10 +0300

    Начал делать скраппер

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-.
+import requests as rq
+from lxml import html
+import os
+
+VZLJOT_PROXY = {
+  'http': 'http://SibiryakovDO:vzlSofia1302@proxy:3128',
+  'https': 'http://SibiryakovDO:vzlSofia1302@proxy:3128',
+}
+
+if (os.environ.get('USERDOMAIN', 'NOT_VZLJOT') != 'VZLJOT'):
+  VZLJOT_PROXY = None
+
+user = 'yakvenalex'
+init_page = 4
+init_linc = 'https://habr.com/en/users/{user}/publications/articles/page{page}/'
+
+# init_linc.format()
+
+for page in range(init_page, 1, -1):
+    resp = rq.get(url=init_linc.format(user=user, page=page))
